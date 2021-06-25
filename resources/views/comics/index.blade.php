285f1ad0dd47ae208ce5c1d0efdf15719e0d270b
andreicismas <andrei_cismas@yahoo.it>
2021-06-25 09:58:12 +0200

    end project

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,29 @@
 @section('content')
    
     <h2>fumetti disponibili</h2>
-    {{-- <a href="{{route('comics.show', $comic->id)}}">singolo fumetto</a> --}}
     <a href="{{ route('comics.create') }}">Aggiungi serie...</a>
+
+    <table>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Titolo</th>
+      <th>Prezzo</th>
+      <th>Date</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    @foreach($comics as $comic)
+    <tr>
+      <td>{{ $comic->id }}</td>
+      <td>{{ $comic->title }}</td>
+      <td>{{ $comic->price }}</td>
+      <td>{{ $comic->sale_date }}</td>
+      <td><a href="{{ route('comics.show', $comic->id) }}">Dettagli...</a></td>
+    </tr>
+    @endforeach
+  </tbody>
+
+</table>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 <h2>dettagli fumetto</h2>
 
+        <em>--id {{ $comic->id }}</em>
+        <h3>titolo -{{ $comic->title }}</h3>
+        <p>descrizione -{{ $comic->description }}</p>
+        <p>prezzo US -{{ $comic->price }}</p>
+        <p>serie -{{ $comic->series }}</p>
+        <p>data uscita -{{ $comic->sale_date }}</p>
+        <p>tipologia -  {{ $comic->type }}</p>
 
-        <h3>titolo -</h3>
-        <p>descrizione -</p>
-        <p>url immagine -</p>
-        <p>prezzo US -</p>
-        <p>serie -</p>
-        <p>data uscita -</p>
-        <p>tipologia -</p>
         <a href="{{ route('comics.index') }}">back in  fumetti</a>
 @endsection
